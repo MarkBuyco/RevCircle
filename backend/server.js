cc6d99f0e31a11930caada4e1a878fd65cb6c584
Mark Buyco <markabadbuyco@gmail.com>
2025-06-01 02:18:22 +0400

    Backend connect and some photos

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Adjust the path to reach login-page from backend
-app.use(express.static(path.join(__dirname, '..', '/login-page')));
-app.use(express.static(path.join(__dirname, '..', 'about-page')));
-app.use(express.static(path.join(__dirname, '..', 'forgot-password')));
-app.use(express.static(path.join(__dirname, '..', 'guidelines-page')));
-app.use(express.static(path.join(__dirname, '..', 'home-page')));
-app.use(express.static(path.join(__dirname, '..', 'profile-page')));
-app.use(express.static(path.join(__dirname, '..', 'signup-page')));  
-
+app.use(express.static(path.join(__dirname, '..')));
 
 // Serve login.html on root
 app.get('/', (req, res) => {
